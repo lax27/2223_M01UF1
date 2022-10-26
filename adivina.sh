36e8e3f69b1856e75658408d1aee4a2197c7cd56
enti <enti@enti.enti>
2022-10-26 10:30:41 +0200

    Juego de adivinar el numero

--- adivina.sh
+++ adivina.sh
@@ -1,12 +1,36 @@
-#/bin/bash
-echo "adivina el numero del 1 al 100"
+#/bin/ba
+NUMERO_RANDOM=$((RANDOM % 100 +1))
+
+echo "adivina un numero del 1 al 100"
+#las variavles son ints
+# NUM=0
+#while [ $NUMERO_RANDOM -ne $Num ]
+#y el if 
+
+#las variavles son strings
+#while [ "$NUMERO_RANDOM" != "$NUM" ]
+
+while [[ $NUMERO_RANDOM -ne $NUM ]]
+do
 
 read NUM
 
-if [ "$NUM" -eq  "3" ]
+if [[ $NUM -gt $NUMERO_RANDOM ]]
 then
-echo "son iguales"
+
+echo "el numero random es menor"
+
+elif [[ $NUM -lt $NUMERO_RANDOM ]]
+then
+
+echo "el numero random es mayor"
+
+else
+echo "es igual"
+
 fi
-	
+
+done
+
 
 
